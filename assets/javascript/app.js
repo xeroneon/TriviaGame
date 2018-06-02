74adbf690ab4b2db4dd4b4778d72aca5f6bdefea
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-01 19:11:09 -0700

    made basis for html and made array of objects to hold trivia questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var questions = [
+    first = {
+        question: "What is the player character called?" ,
+        rightanswer: "Interloper",
+        wrong1: "Introloper",
+        wrong2: "Interceptor",
+        wrong3: "Traveller"
+    },
+    second = {
+        question: "What element powers launch thrusters",
+        rightanswer: "Plutonium",
+        wrong1: "Nickel",
+        wrong2: "Emeril",
+        wrong3: "Carbon"
+    }
+
+]
